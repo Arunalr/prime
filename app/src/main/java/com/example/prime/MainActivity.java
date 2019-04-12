66ddcc41e3d57e7c0f750882f38eaba8c684b668
ACER <arunalrklatharajan@gmail.com>
2019-04-12 21:35:10 +0530

    commit2

---
+++ app/src/main/java/com/example/prime/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.prime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText num;
+    Button btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click(View view){
+        Toast.makeText(this,"hai",Toast.LENGTH_LONG).show();
+    }
+}
